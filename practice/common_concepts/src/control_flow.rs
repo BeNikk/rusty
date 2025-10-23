15f73cff3396ac9ce418d4c6f609c31d2f960224
BeNikk <bhattnik442@gmail.com>
2025-10-23 16:53:57 +0530

    complete:common programming concepts

---
+++ practice/common_concepts/src/control_flow.rs
@@ -0,0 +1,37 @@
+pub fn eligible_to_vote(age: u32) {
+    if age > 18 {
+        println!("you are eligible to vote");
+    } else if age < 18 {
+        println!("you are not eligible to vote");
+    } else {
+        println!("you are eligble to vote this year, now make voting card if not already");
+    }
+}
+pub fn sum(a: u32, b: u32) -> u32 {
+    a + b
+}
+
+pub fn iteration(x: u32) {
+    let mut y = 0;
+    loop {
+        if y >= x {
+            break;
+        }
+        println!("Looping inside the iteration function {}", y);
+        y += 1;
+    }
+}
+
+pub fn while_iteration(x: u32) {
+    let mut y = 0;
+    while x > y {
+        println!("the value of iteration number is {}", y);
+        y += 1;
+    }
+}
+
+pub fn for_iteration_in_array(a: &[i32]) {
+    for element in a {
+        println!("{}", element);
+    }
+}
--- practice/common_concepts/src/main.rs
+++ practice/common_concepts/src/main.rs
@@ -1,3 +1,5 @@
+mod control_flow;
+
 fn main() {
     //declaring a mutable var;
     let mut x = 10;
@@ -8,7 +10,27 @@ fn main() {
     println!("the variable that is shadowed is {x}");
 
     // integer overflow and wrapping
-    let y: u8 = 256; // at compile time this should give error but at run time, it will wrap to
-                     // 0->256, 1->257 like that.
-    println!("the value of y is {y}");
+    //let y: u8 = 256; // at compile time this should give error but at run time, it will wrap to
+    // 0->256, 1->257 like that.
+    //println!("the value of y is {y}");
+
+    // tuples
+    let mut y = (2, 4, "String");
+    y.0 += 2;
+    let (a, b, c) = y;
+    println!("{a} {b} {c}");
+
+    let array = [1, 2, 3, 4, 5];
+    // functions
+    let first = 5;
+    let second = 6;
+    let result = control_flow::sum(first, second);
+    println!("{result}");
+
+    // if else
+    let age = 18;
+    control_flow::eligible_to_vote(age);
+    control_flow::iteration(4);
+    control_flow::while_iteration(5);
+    control_flow::for_iteration_in_array(&array);
 }
