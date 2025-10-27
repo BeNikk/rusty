8651d6c17061e91cfbbec743b94ccee8473563e9
BeNikk <bhattnik442@gmail.com>
2025-10-27 20:55:19 +0530

    minor:struct changes

--- practice/structures/src/main.rs
+++ practice/structures/src/main.rs
@@ -1,9 +1,17 @@
+mod tuple_struct;
 mod user;
-
 fn main() {
-    let user1 = user::User {
-        name: String::from("nikhil"),
+    let mut user1 = user::User {
+        name: String::from("rust"),
         eligible: true,
     };
+    user1.name = String::from("rust and rustlings");
     println!("{} {}", user1.name, user1.eligible);
+    let user2 = user::User {
+        name: user1.name,
+        eligible: false,
+    };
+    println!("{} {}", user2.name, user2.eligible);
+    let white = tuple_struct::Color(255, 255, 255);
+    println!("{}, {},  {}", white.0, white.1, white.2);
 }
