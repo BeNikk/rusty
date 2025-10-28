32a2fe3e50901bcabbe0394b058045afd118bc77
BeNikk <bhattnik442@gmail.com>
2025-10-28 11:37:13 +0530

    enums:init

--- practice/enumerations/src/main.rs
+++ practice/enumerations/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let rect = Shape::Rectangle(2.0, 3.0);
+    println!("the area of rectangle is {}", calculate_area(rect));
+    let circle = Shape::Circle(10.0);
+    println!("the area of circle is {}", calculate_area(circle));
+}
+enum Shape {
+    Rectangle(f64, f64),
+    Circle(f64),
+}
+
+fn calculate_area(shape: Shape) -> f64 {
+    match shape {
+        Shape::Rectangle(a, b) => a * b,
+        Shape::Circle(a) => 3.14 * a * a,
+    }
 }
