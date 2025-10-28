104b2b96868647a63dc7fb074c00ee66a43ecbcb
BeNikk <bhattnik442@gmail.com>
2025-10-28 18:51:07 +0530

    minor:enum

--- practice/enumerations/src/result.rs
+++ practice/enumerations/src/result.rs
@@ -1,13 +1,15 @@
 use std::fs;
-
-pub fn read_file(path: &str) {
+use std::io;
+pub fn read_file(path: &str) -> Option<String> {
     let file_result = fs::read_to_string(path);
     match file_result {
         Ok(file_content) => {
             println!("file read successfuly {:?}", file_content);
+            Some(file_content)
         }
         Err(error) => {
             println!("error in reading the file {:?}", error);
+            None
         }
     }
 }
