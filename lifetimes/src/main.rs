79dc4478c4eff168779026a308ab5a131e1ec70e
BeNikk <bhattnik442@gmail.com>
2025-10-29 20:04:33 +0530

    init:lifetimes

---
+++ lifetimes/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("Lifetimes");
+
+    let a = String::from("hellow");
+    let b = String::from("World");
+    let longest = longest(&a, &b);
+    println!("{}", longest);
+}
+//since the compiler does not know
+//the scope till which the variable would be available
+//so it asks us to specify until which part the argument is valid.
+//this wierd syntax is just representing that the answer reference is
+//valid until the reference of both the argument is valid, like till when the
+//intersection of both variable is valid, only till there, the return type is valid
+fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.len() > b.len() {
+        a
+    } else {
+        b
+    }
+}
