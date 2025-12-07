b74c1a5c0e823c39738d3511c7c36bec4815add6
BeNikk <bhattnik442@gmail.com>
2025-12-07 17:37:30 +0530

    assert, panic and result handling

--- bank/src/lib.rs
+++ bank/src/lib.rs
@@ -9,7 +9,13 @@ impl SavingAccount {
         self.balance
     }
     pub fn deposit(&mut self, amount: i32) {
+        if amount < 0 {
+            panic!("Cannot deposit negative amount");
+        }
         self.balance += amount;
+    }
+    pub fn transfer(&self, acc_number: u32, amount: i32) -> Result<String, String> {
+        Ok(format!("Transferred ${amount} to {acc_number}"))
     }
 }
 
@@ -26,5 +32,29 @@ mod tests {
         let mut account = SavingAccount::new();
         account.deposit(100);
         assert_eq!(account.get_balance(), 100);
+    }
+    #[test]
+    fn should_not_equal() {
+        let account = SavingAccount::new();
+        assert_ne!(account.get_balance(), 200);
+    }
+    #[test]
+    fn assert_macro() {
+        let account = SavingAccount::new();
+        assert!(account.get_balance() == 0);
+    }
+    #[test]
+    fn should_transfer_money() -> Result<(), String> {
+        let mut account = SavingAccount::new();
+        account.deposit(200);
+        account.transfer(123, 50)?;
+        Ok(())
+        // Call transfer(...) -> If it returns Ok(value) → continue,If it returns Err(error) → immediately return that error from should_transfer_money
+    }
+    #[test]
+    #[should_panic]
+    fn should_panic_if_deposit_is_negative() {
+        let mut account = SavingAccount::new();
+        account.deposit(-1);
     }
 }
